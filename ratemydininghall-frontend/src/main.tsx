5378556d0f4a6119ff30991c42f7c62860e97bf0
neiljya <neil.j.wu@gmail.com>
2025-10-06 23:20:45 -0700

    styling fix attempt

--- ratemydininghall-frontend/src/main.tsx
+++ ratemydininghall-frontend/src/main.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import ReviewPage from './pages/ReviewPage.tsx';
-import '@globalStyles/theme/theme.module.css';
-import './global-styles/theme/theme.module.css';
+import theme from '@globalStyles/theme/theme.module.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <div className={theme.themeVars}>
         <ReviewPage />
+      </div>
     </Provider>
   </StrictMode>,
 )
